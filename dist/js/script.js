67464d3496ae898908ac263e27ccbef21db72d32
Arjayieeee <federioarjay16@gmail.com>
2023-05-10 15:33:47 +0800

    up

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,25 @@ toggleMenu.addEventListener("click", () => {
   headNav.classList.toggle("open");
   kamote.classList.toggle("open");
 });
+
+
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
+
+//const header = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 100) {
+    header.classList.add("blue");
+   // brandimg.style="background: red";
+  } else {
+    header.classList.remove("blue");
+  }
+});
+
+//const brandimg = document.querySelector(".branding img");
